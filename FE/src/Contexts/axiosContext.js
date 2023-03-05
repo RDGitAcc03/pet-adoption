042c8fbeaffe15eee0dd873b0a0541480072dd55
RD <randahn03@gmail.com>
2023-03-05 20:14:52 +0200

    built  getServerUrl func in axiosContext.js

--- FE/src/Contexts/axiosContext.js
+++ FE/src/Contexts/axiosContext.js
@@ -1,7 +1,12 @@
 const axios = require("axios");
 
+const getServerUrl = () => {
+  if (process.env.NODE_ENV === 'production') return 'https://example-petadoption.onrender.com'
+  return "http://localhost:8080";
+}
+
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: getServerUrl(),
   // timeout: 1000,
   withCredentials: true ,
 });
